2bdffb335e6b7e9cfd259ba23622b3cb5254d7ce
BogdanaOst <bogdanochkaost@gmail.com>
2017-07-06 14:35:58 +0300

    Redirect to main in case 404

--- WebWeather/Global.asax.cs
+++ WebWeather/Global.asax.cs
@@ -17,5 +17,32 @@ namespace WebWeather
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            HttpContext oHttpContext;
+            Exception oException;
+
+            oHttpContext = HttpContext.Current;
+
+            oException = oHttpContext.Server.GetLastError();
+
+            if (oException is HttpException)
+            {
+                switch ((oException as HttpException).GetHttpCode())
+                {
+                    case 404:
+
+                        oHttpContext.Response.StatusCode = 404;
+                        oHttpContext.Response.StatusDescription = "Not Found";
+                       // oHttpContext.Response.Charset = "windows-1251";
+
+                        oHttpContext.Server.TransferRequest("~/Weather/");
+
+                        oHttpContext.Server.ClearError();
+                        break;
+                }
+            }
+        }
+
     }
 }
