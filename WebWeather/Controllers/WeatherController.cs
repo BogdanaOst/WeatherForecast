d94ed45be0c5b4dc7fa82e1b4aa33bdcd59c2a3b
BogdanaOst <bogdanochkaost@gmail.com>
2017-07-11 23:12:36 +0300

    ui updates with selectedcity

--- WebWeather/Controllers/WeatherController.cs
+++ WebWeather/Controllers/WeatherController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using BLL.Managers;
 using DAL.DTOs;
+using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using WebWeather.Models;
 using WebWeather.Services;
@@ -12,19 +14,25 @@ namespace WebWeather.Controllers
 
         IForecastService forecastService;
         IHistoryManager historyManager;
-
+        ISelectedCityManager cityManager;
+        List<SelectedCityViewModel> selectedCitiesList;
         static Forecast forecast;
 
-        public WeatherController(IForecastService iservice, IHistoryManager ihistoryManager)
+        public WeatherController(IForecastService iservice, IHistoryManager ihistoryManager, ISelectedCityManager icityManager)
         {
             forecastService = iservice;
             historyManager = ihistoryManager;
+            cityManager = icityManager;
+            var citiesDtos = cityManager.GetAll();
+            Mapper.Initialize(cfg => cfg.CreateMap<SelectedCityDTO, SelectedCityViewModel>());
+            selectedCitiesList = Mapper.Map<List<SelectedCityDTO>, List<SelectedCityViewModel>>(citiesDtos);
+
+            ViewBag.Cities = selectedCitiesList;
         }
 
         // GET: Weather
         public ActionResult Index()
         {
-            
             return View();
         }
 
@@ -39,6 +47,8 @@ namespace WebWeather.Controllers
             
                 forecast = forecastService.GetForecast(new Parametrs() { CityName = forecast.City, NumOfDays = parametrs.NumOfDays });
 
+            try
+            {
                 HistoryViewModel model = new HistoryViewModel()
                 {
                     City = forecast.City,
@@ -48,10 +58,15 @@ namespace WebWeather.Controllers
                     Icon = forecast.GetDailyList()[0].Icon
                 };
 
+
                 Mapper.Initialize(cfg => cfg.CreateMap<HistoryViewModel, HistoryDTO>());
                 var historyDto = Mapper.Map<HistoryViewModel, HistoryDTO>(model);
                 historyManager.Add(historyDto);
-            
+            }
+            catch (Exception ex)
+            {
+                return View(forecast);
+            }
             return View(forecast);
         }
         public ActionResult OtherCity()
